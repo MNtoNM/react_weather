1787d4da8648fb142eb65320523301b9a61c2337
Jonathan Dueck <jdd612@gmail.com>
2017-01-30 15:51:51 -0700

    employ grid system

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) =>  {
   return (
-    <h3>Examples</h3>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=minneapolis'>Minneapolis</Link>
+        </li>
+        <li>
+          <Link to='/?location=Edina'>Edina</Link>
+        </li>
+      </ol>
+    </div>
   )
 };
 
