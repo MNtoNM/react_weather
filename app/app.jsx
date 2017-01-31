0f53bc12c217c913c1dd096f29d84a85822db686
Jonathan Dueck <jdd612@gmail.com>
2017-01-31 13:57:43 -0700

    Add error modal

--- app/app.jsx
+++ app/app.jsx
@@ -4,12 +4,10 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
 var Weather = require('Weather');
 var About = require('About');
-var Nav = require('Nav');
 var Examples = require('Examples');
-var WeatherForm = require('WeatherForm');
 
 // Load foundation
-require('style!css!foundation-sites/dist/foundation.min.css');
+require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 
 ReactDOM.render(
